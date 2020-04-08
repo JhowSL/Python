379aaa318069bdedcf28cb1d6e2ff7c89469db22
Jonathan Souza Lima <61327152+JhowSL@users.noreply.github.com>
2020-04-08 19:38:18 -0300

    Delete ex005 - Antecessor e sucessor.py

--- ex005 - Antecessor e sucessor.py
+++
@@ -1,8 +0,0 @@
-# Desafio 005 - Antecessor e sucessor
-
-# Entrada
-n = int(input('Digite um número: '))
-# Saida
-print('O antecessor de {} é {} e o seu posterior é {}.'.format(n, n-1, n+1))
-# Sair do programa
-input()
